b05e6c435f95c2a831b6972965ed24a6f82006fe
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-11-14 14:08:01 +0100

    Build by copying custom Makefile and linking objects instead of lib

--- build.sh
+++ build.sh
@@ -3,15 +3,14 @@ FC=gfortran
 KROME=krome
 PROJECT=c_example
 KROME_OUTPUT_DIR=krome/build_$PROJECT
-LIBNAME=krome
 
 set -x
 
+# Run krome
 (cd krome && python2 krome -test=hello -project=$PROJECT -interfaceC)
 
-make -C $KROME_OUTPUT_DIR cinterface gfortran
-rm $KROME_OUTPUT_DIR/test.o # we don't want this in our shared library
-$FC $KROME_OUTPUT_DIR/*.o -o $KROME_OUTPUT_DIR/lib${LIBNAME}.so -shared -llapack
+# Build krome objects (with custom Makefile)
+cp c_example/krome_custom_Makefile $KROME_OUTPUT_DIR
+make -f krome_custom_Makefile -C $KROME_OUTPUT_DIR gfortran cinterface
 
-gcc -o run c_example/main.c -I$KROME_OUTPUT_DIR -L$KROME_OUTPUT_DIR -l$LIBNAME -lm -lgfortran
-
+gcc -o run c_example/main.c $KROME_OUTPUT_DIR/*.o -I$KROME_OUTPUT_DIR -lm -lgfortran
---
+++ clean.sh
@@ -0,0 +1,1 @@
+rm -r krome/build*/*
