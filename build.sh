e72c8817a581620e7fdb25902ba32839c30f7076
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-11-14 13:13:06 +0100

    First version of build script

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
-echo build file is empty for now
+FC=gfortran
+
+KROME=krome
+PROJECT=c_example
+KROME_OUTPUT_DIR=krome/build_$PROJECT
+LIBNAME=krome
+
+set -x
+
+(cd krome && python2 krome -test=hello -project=$PROJECT -interfaceC)
+
+make -C $KROME_OUTPUT_DIR cinterface gfortran
+rm $KROME_OUTPUT_DIR/test.o # we don't want this in our shared library
+$FC $KROME_OUTPUT_DIR/*.o -o $KROME_OUTPUT_DIR/lib${LIBNAME}.so -shared -llapack
+
+gcc -o run c_example/main.c -I$KROME_OUTPUT_DIR -L$KROME_OUTPUT_DIR -l$LIBNAME -lm -lgfortran
+
