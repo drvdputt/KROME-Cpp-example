36a3659f9d9feff3a6aca3bb19b49a1df5303b67
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-11-14 11:45:31 +0100

    Add example C script

---
+++ c_example/main.c
@@ -0,0 +1,31 @@
+#include "krome.h"
+#include "krome_user.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	double Tgas, dt, t;
+	double* x = (double*)calloc(krome_nmols, sizeof(double));
+
+	x[krome_idx_FK1] = 1.;
+
+	Tgas = 100.;
+	dt = 1.e-5;
+	t = 0.;
+
+	while(t < 5.)
+	{
+		dt = dt * 1.1;
+		krome(x, &Tgas, &dt);
+		printf("%f ", t);
+		for (i = 0; i < krome_nmols; i += 1)
+			printf("%f ", x[i]);
+		printf("\n");
+		t = t + dt;
+	}
+
+	printf("Test OK");
+}
